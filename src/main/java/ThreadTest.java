4cd65391ce1f8958aa6b610200feafc7105d3e8b
wangcheng_global <wangcheng_global@1633.com>
2020-01-16 23:22:50 +0800

    测试线程类

---
+++ src/main/java/ThreadTest.java
@@ -0,0 +1,30 @@
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+		MyThread t1 = new MyThread("zhangs");
+		t1.start();
+		for (int i = 0; i < 20; i++) {
+			if (i%2==0) {
+				System.out.println(Thread.currentThread().getName() + ":" + i);
+			}
+		}
+	}
+
+}
+
+class MyThread extends Thread{
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 20; i++) {
+			if (i%2==0) {
+				System.out.println(Thread.currentThread().getName() + ":" + i);
+			}
+		}
+	}
+	public MyThread(String name) {
+		super(name);
+	}
+	
+}
